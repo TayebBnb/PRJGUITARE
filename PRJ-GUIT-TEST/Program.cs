0af6e1ae6d9c7acbb6ff22683221ba165d309d0b
TayebBnb <101246263+TayebBnb@users.noreply.github.com>
2023-05-21 21:00:40 +0200

    Add files via upload
    commitentt

---
+++ PRJ-GUIT-TEST/Program.cs
@@ -0,0 +1,16 @@
+﻿using LIB_LUTHIER_TEST;
+
+namespace PRJ_GUIT_TEST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            C_BASE oui = new C_BASE();
+
+            var test = oui.Get_All_Bois();
+
+            Console.WriteLine(test.Count);
+        }
+    }
+}
